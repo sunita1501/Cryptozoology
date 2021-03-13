3d58cbf3fb34a1ccb2dffb00b748747bd33df6cf
sunita1501 <sunita.kumari@cognizant.com>
2021-03-13 12:27:02 -0500

    First Commit

---
+++ src/main/java/com/galvanize/cognizant/Cyptozoology/CyptozoologyApplication.java
@@ -0,0 +1,13 @@
+package com.galvanize.cognizant.Cyptozoology;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CyptozoologyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CyptozoologyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/galvanize/cognizant/Cyptozoology/CyptozoologyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.galvanize.cognizant.Cyptozoology;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CyptozoologyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
