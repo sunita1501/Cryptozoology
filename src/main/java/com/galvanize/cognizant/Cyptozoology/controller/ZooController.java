685e4377b55cf16e7b2e9b57457ab35db793689b
sunita1501 <sunita.kumari@cognizant.com>
2021-03-13 14:17:16 -0500

    Green on get animal details

--- src/main/java/com/galvanize/cognizant/Cyptozoology/controller/ZooController.java
+++ src/main/java/com/galvanize/cognizant/Cyptozoology/controller/ZooController.java
@@ -1,10 +1,7 @@
 package com.galvanize.cognizant.Cyptozoology.controller;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/zoo")
@@ -14,6 +11,12 @@ public class ZooController {
     @ResponseStatus(HttpStatus.CREATED)
     public void postAnimal(){
 
+
+    }
+
+    @GetMapping("/animal")
+    @ResponseStatus(HttpStatus.OK)
+    public void getAnimal(){
 
     }
 
--- src/test/java/com/galvanize/cognizant/Cyptozoology/controller/ZooControllerIT.java
+++ src/test/java/com/galvanize/cognizant/Cyptozoology/controller/ZooControllerIT.java
@@ -4,10 +4,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 
 @SpringBootTest
@@ -20,8 +22,10 @@ public class ZooControllerIT {
     @Test
     public void postAnimal() throws Exception {
 
-        mockmvc.perform(post("/zoo/animal")).andExpect(status().isCreated());
+        mockmvc.perform(post("/zoo/animal").contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated());
 
+        mockmvc.perform(get("/zoo/animal")).andExpect(status().isOk());
 
     }
 }
